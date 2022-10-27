30ab091b196bf44a7e76569b0655bdf581e11392
Yashjeet <yash@napses.com>
2022-10-27 16:48:51 +0530

    added ui project generation

--- index.js
+++ index.js
@@ -7,13 +7,40 @@ const CURR_DIR = process.cwd();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 import ora from 'ora';
-const templateProjectName = "api-framework";
-const spinner = ora('Loading')
-spinner.color = 'yellow';
-spinner.text = 'Loading';
-const QUESTIONS = [
+import * as subProcess from 'child_process'
+import chalk from 'chalk';
+
+const apiProjectTemplate = "api-framework";
+const uiProjectTemplate = "ui-framework"
+const spinner = ora('Loading');
+
+//Welcome Message
+const log = console.log;
+log(chalk.red('Lets Begin'));
+
+const projectTypeQuestion = [
     {
-        name: 'project-name',
+        message: 'What you want?',
+        type: 'list',
+        name: "projectType",
+        choices: [
+            {
+                name: 'API Project',
+                value: 'api_project',
+                description: 'Mvp Rocket API project',
+            },
+            {
+                name: 'UI Project',
+                value: 'ui_project',
+                description: 'Mvp Rocket UI Project',
+            }
+        ]
+    }
+];
+
+const projectNameQuestion = [
+    {
+        name: 'project_name',
         type: 'input',
         message: 'Project name:',
         validate: function (input) {
@@ -23,15 +50,38 @@ const QUESTIONS = [
     }
 ];
 
-inquirer.prompt(QUESTIONS).then(answers => {
-    spinner.start();
-    const projectName = answers['project-name'];
-    const templatePath = `${__dirname}/${templateProjectName}`;
-    fs.mkdirSync(`${CURR_DIR}/${projectName}`);
-    createDirectoryContents(templatePath, projectName, projectName);
+inquirer.prompt(projectTypeQuestion).then(projectTypeAnswer => {
+    const projectType = projectTypeAnswer['projectType'];
+    inquirer.prompt(projectNameQuestion).then(projectNameAnswer => {
+        const projectName = projectNameAnswer['project_name']
+        spinner.start();
+        if (projectType === 'api_project') {
+            const templatePath = `${__dirname}/${apiProjectTemplate}`;
+            fs.mkdirSync(`${CURR_DIR}/${projectName}`);
+            createApiProjectDirectoryContents(templatePath, projectName, projectName, apiProjectTemplate);
+        }
+        if (projectType === 'ui_project') {
+            const templatePath = `${__dirname}/${uiProjectTemplate}`;
+            fs.mkdirSync(`${CURR_DIR}/${projectName}`);
+            createUiProjectDirectoryContents(templatePath, projectName, projectName);
+        }
+        updateVersion(projectName);
+    });
 });
 
-function createDirectoryContents(templatePath, newProjectPath, projectName) {
+function updateVersion(projectName) {
+    const writePath = `${CURR_DIR}/${projectName}/`;
+    subProcess.exec(`cd ${writePath} && npm version 1.0.0 --no-git-tag-version --allow-same-version=true`, (err, stdout, stderr) => {
+        if (err) {
+            console.error(err)
+            console.log(`Failed to update package version`)
+            process.exit(1)
+        }
+        spinner.stop();
+    })
+}
+
+function createApiProjectDirectoryContents(templatePath, newProjectPath, projectName) {
     const filesToCreate = fs.readdirSync(templatePath);
 
     filesToCreate.forEach(file => {
@@ -49,9 +99,31 @@ function createDirectoryContents(templatePath, newProjectPath, projectName) {
             fs.writeFileSync(writePath, updateContent, 'utf8');
         } else if (stats.isDirectory()) {
             fs.mkdirSync(`${CURR_DIR}/${newProjectPath}/${file}`);
-            createDirectoryContents(`${templatePath}/${file}`, `${newProjectPath}/${file}`, projectName);
+            createApiProjectDirectoryContents(`${templatePath}/${file}`, `${newProjectPath}/${file}`, projectName);
         }
     });
-    spinner.stop();
+}
+
+function createUiProjectDirectoryContents(templatePath, newProjectPath, projectName) {
+    const filesToCreate = fs.readdirSync(templatePath);
+
+    filesToCreate.forEach(file => {
+        const origFilePath = `${templatePath}/${file}`;
+        const stats = fs.statSync(origFilePath);
+        if (stats.isFile()) {
+            const contents = fs.readFileSync(origFilePath, 'utf8');
+            const writePath = `${CURR_DIR}/${newProjectPath}/${file}`;
+            let updateContent;
+            if (file === 'package.json') {
+                updateContent = contents.split(/namma-frontend-framework/).join(projectName)
+            } else {
+                updateContent = contents.split(/<namma-frontend-framework>/).join(projectName)
+            }
+            fs.writeFileSync(writePath, updateContent, 'utf8');
+        } else if (stats.isDirectory()) {
+            fs.mkdirSync(`${CURR_DIR}/${newProjectPath}/${file}`);
+            createApiProjectDirectoryContents(`${templatePath}/${file}`, `${newProjectPath}/${file}`, projectName);
+        }
+    });
 }
 
